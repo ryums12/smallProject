87790f2f303d04452c0608f26f65b80832bfa7ba
minsongryu <ryums0712@archivsoft.com>
2020-07-09 15:28:08 +0900

    메인 페이지 UI 추가 수정, 기타 설정 파일 이동

--- BookMark/src/main/java/com/ryums/bookmark/config/SecurityConfig.java
+++ BookMark/src/main/java/com/ryums/bookmark/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ryums.bookmark;
+package com.ryums.bookmark.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
