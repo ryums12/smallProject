bb32980746131e465e29ca7099ffa86eb41b446a
ryums12 <nbfaker@naver.com>
2020-09-01 23:25:42 +0900

    [UI] UI 수정

--- BookMark/src/main/resources/static/js/main.js
+++ BookMark/src/main/resources/static/js/main.js
@@ -33,26 +33,28 @@ function fncSetMarkList(data, response) {
         //row 시작
         listHtml = i == 4 ? listHtml + "<div class='row mt-4'>" : listHtml;
 
-        listHtml += "<div class='col-md-3 mt-4'>"
-                        + "<div class='card'>"
-                            + "<img src='" + markList[i].imgUrl + "' class='custom-thumbnail'>"
-                            + "<hr>"
+        listHtml += "<div class='col-md-3 mt-3'>"
+                        + "<div class='card border-secondary text-center'>"
+                            + "<img src='" + markList[i].imgUrl + "' class='card-img-top' alt='tag thumbnail'>"
+                            + "<h6 class='card-header'>[" + markList[i].tagName + "]</h6>"
+                            + "<div>"
+                                + "<p class='card-text'>" + markList[i].markTitle + "</p>"
+                            + "</div>"
                             + "<div class='caption text-center'>"
-                                + "<h6>[" + markList[i].tagName + "]" + markList[i].markTitle + "</h6>"
                                 + "<button class='btn btn-primary' "
-                                + "data-url='" + markList[i].markUrl + "'"
-                                + "onclick='fncGoToMarkLink(this)'>이동</button>"
-                                + "<button class='btn btn-success'"
-                                + "data-toggle='modal'"
-                                + "data-remote='/mark/"+ markList[i].markIdx + "'"
-                                + "data-target='#modal-update-mark'"
-                                + "onclick='fncOpenUpdateMarkModal(this)'>수정</button>"
+                                        + "data-url='" + markList[i].markUrl + "' "
+                                        + "onclick='fncGoToMarkLink(this)'>이동</button>"
+                                + "<button class='btn btn-success' "
+                                        + "data-toggle='modal' "
+                                        + "data-remote='/mark/"+ markList[i].markIdx + "' "
+                                        + "data-target='#modal-update-mark' "
+                                        + "onclick='fncOpenUpdateMarkModal(this)'>수정</button>"
                             + "</div>"
                         + "</div>"
                      + "</div>";
 
         //row 닫기
-        listHtml = i == 3 || i == 7 ? listHtml + "</div>" : listHtml;
+        listHtml = i === 3 || i === 7 ? listHtml + "</div>" : listHtml;
     }
 
     container.innerHTML = listHtml;
